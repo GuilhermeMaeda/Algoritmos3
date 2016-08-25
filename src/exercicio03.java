9cb3e7656a76699503a91399d70805ee94af0140
Joao Paulo Nunes Marques <joaopaulonunesm@hotmail.com>
2016-08-24 22:42:21 -0300

    Correções exercicio 01 e 03
    Correções.

--- src/exercicio03.java
+++ src/exercicio03.java
@@ -1,5 +1,33 @@
-//encontrar o valor da posição lida por um documento
+import java.io.File;
+import javax.swing.JFileChooser;
 
 public class exercicio03 {
+
+	public static void main(String[] args) {
+
+		acaoExercicio03 eUm = new acaoExercicio03();
+		eUm.runExerciseOne(getPathFile());
+
+	}
+
+	public static final String getPathFile() {
+
+		final JFileChooser fc = new JFileChooser();
+		int returnValue = fc.showOpenDialog(null);
+
+		if (returnValue == JFileChooser.APPROVE_OPTION) {
+
+			File seleFile = fc.getSelectedFile();
+			return seleFile.getAbsolutePath();
+
+		} else {
+			
+			System.exit(0);
+			
+		}
+
+		return null;
+
+	}
 
 }
